6adb811dbb6e5618db31cf339ad0533628f6d819
Stijn Klomp <stijnklomp1@hotmail.com>
2026-06-13 21:35:35 +0100

    Suppress hydration warning because of Chrome remote debugging attribute injection

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -21,7 +21,7 @@ export default async function LocaleLayout({
 	const messages = await getMessages()
 
 	return (
-		<html lang={locale}>
+		<html lang={locale} suppressHydrationWarning>
 			<body>
 				<NextIntlClientProvider locale={locale} messages={messages}>
 					<ThemeProvider>
